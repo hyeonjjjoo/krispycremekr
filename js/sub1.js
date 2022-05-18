3beb3999546cce4f212e59737d2f7966bad6b64c
hyeon joo <guswn440@naver.com>
2022-05-18 23:29:30 +0900

    20220518

--- js/sub1.js
+++ js/sub1.js
@@ -1,6 +1,34 @@
 $(function(){
-    $('.cntWrap li').click(function(){
-        $('.cntWrap li').removeClass('on')
+    var li = $('.cntWrap li');
+    var artGroup = $('.artTop .artGroup');
+    var btnLeft = $('.dirBtn .btn:nth(0)');
+    var btnRight = $('.dirBtn .btn:nth(1)');
+    var i = 0;
+    li.click(function(){
+        li.removeClass('on')
         $(this).addClass('on')
     });
+    
+    var wd = $(window).width();
+    if(wd <= 420){
+        artGroup.css({
+            marginLeft : '-100%',          
+        });
+        i = 1;
+        btnRight.click(function(){
+            i++;
+            i = i%3;
+            artGroup.animate({
+                marginLeft : -100 * i+'%'
+            });
+            
+        });
+        btnLeft.click(function(){
+            i--;
+            i = (i+3)%3;
+            artGroup.animate({
+            marginLeft : -100 * i+'%'
+        });
+        });
+    }
 });
