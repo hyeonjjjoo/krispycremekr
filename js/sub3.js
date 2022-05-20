a3da9afac3e67ed1fb0b749112e542f89fbcff07
hyeon joo <guswn440@naver.com>
2022-05-20 23:38:24 +0900

    20220520

--- js/sub3.js
+++ js/sub3.js
@@ -7,5 +7,16 @@ $(function(){
         var txt = $(this).text()
         $('.boxLeft h4').text(txt);
     });
+
+    var li = $('.mapBox li');
+    li.click(function(){
+        li.removeClass('on');
+        $(this).addClass('on');
+    });
+    var tab = $('.tab li');
+    tab.click(function(){
+        tab.removeClass('on');
+        $(this).addClass('on');
+    });
     
 });
